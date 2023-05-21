37e5128e8e9a052d878cc78ef88f0de71f5f4692
EnricMM0 <enric.martinez04@estudiant.upf.edu>
2023-05-21 12:46:41 +0200

    Fitxer text i llista usuaris

--- Sources/main.c
+++ Sources/main.c
@@ -6,11 +6,11 @@
 #define MAX_FRIENDS 50
 
 typedef struct{
-    char user;
+    char user[MAX_LENGTH];
     int age;
     char mail[MAX_LENGTH];
     char loc[MAX_LENGTH];
-    char hobby[5];
+    char hobby[5][MAX_LENGTH];
     int friends[MAX_FRIENDS];
 } User;
 typedef struct {
@@ -22,77 +22,19 @@ List readUsersFromFile(const char* filename) {
     List list;
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
-        printf("Failed to open file: %s\n", filename);
+        printf("No s'ha pogut obrir el fitxer %s\n", filename);
         list.num = 0;
         return list;
     }
-
-    char line[MAX_LENGTH];
-    char* token;
+    char line[200];
 
     list.num = 0;
 
     while (fgets(line, sizeof(line), file) != NULL) {
-        if (line[strlen(line) - 1] == '\n')
-            line[strlen(line) - 1] = '\0';
-
         User user;
-
-        token = strtok(line, ",");
-        if (token == NULL) {
-            printf("Invalid line format: %s\n", line);
-            continue;
-        }
-        strncpy(&user.user, token, sizeof(user.user));
-
-        token = strtok(NULL, ",");
-        if (token == NULL) {
-            printf("Invalid line format: %s\n", line);
-            continue;
-        }
-        user.age = atoi(token);
-
-        token = strtok(NULL, ",");
-        if (token == NULL) {
-            printf("Invalid line format: %s\n", line);
-            continue;
-        }
-        strncpy(user.mail, token, sizeof(user.mail));
-        //loc
-        token = strtok(NULL, ",");
-        if (token == NULL) {
-            printf("Invalid line format: %s\n", line);
-            continue;
-        }
-        strncpy(user.loc, token, sizeof(user.loc));
-
-        //hobby
-        token = strtok(NULL, "(");
-        if (token == NULL) {
-            printf("Invalid line format: %s\n", line);
-            continue;
-        }
-
-        token = strtok(NULL, ",");
-        for (int i = 0; i < 5 && token != NULL; i++) {
-            while (*token == ' ')
-                token++;
-            int len = strlen(token);
-            while (len > 0 && token[len - 1] == ' ')
-                len--;
-            strncpy(&user.hobby[i], token, len);
-            token = strtok(NULL, ",");
-        }
-
-        //final hobby
-        token = strtok(NULL, ")");
-        if (token == NULL) {
-            printf("Invalid line format: %s\n", line);
-            continue;
-        }
-
-        //usuari afegit a la llista
-        list.users[list.num++] = user;
+        sscanf(line,"%[^,],%d,%[^,],%[^,],(%[^,],%[^,],%[^,],%[^,],%[^)])",user.user, &user.age, user.mail, user.loc,user.hobby[0],user.hobby[1],user.hobby[2],user.hobby[3],user.hobby[4] );
+        list.users[list.num] = user;
+        list.num++;
     }
 
     fclose(file);
@@ -100,41 +42,45 @@ List readUsersFromFile(const char* filename) {
 }
 
 void print_menu(){
-    printf("1.Insertar usuari nou\n2.Mostrar usuaris existents\n3.Seleccionar usuari\n4.Sortir\n");
+    printf("\n1.Insertar usuari nou\n2.Mostrar usuaris existents\n3.Seleccionar usuari\n4.Sortir\n");
 }
-int menu(){
-    List list = readUsersFromFile(".../Practica EDA2/users.txt");
-
+int menu(List list){
     int opt = -1;
     while (opt == -1){
         print_menu();
         scanf("%d", &opt);
         if (opt == 1){
             //Insertar usuari nou
-            int newuser = -1;
-            int i = 0;
+            User newuser;
             printf("Usuari:");
-            scanf("%s", &list.users[list.num].user);
+            scanf("%s", newuser.user);
             printf("\nEdat:");
-            scanf("%d",  &list.users[list.num].age);
+            scanf("%d",  &newuser.age);
             printf("\nMail:");
-            scanf(" %[^\n]",  &list.users[list.num].mail);
+            scanf(" %[^\n]",  newuser.mail);
             printf("\nUbicacio:");
-            scanf(" %[^\n]",  &list.users[list.num].loc);
+            scanf(" %[^\n]",  newuser.loc);
             printf("\n5 Hobbies:");
             for (int j = 0; j < 5; j++) {
-                scanf("%s",  &list.users[list.num].hobby[j]);
+                scanf("%s",  newuser.hobby[j]);
             }
             for (int j = 0; j < MAX_FRIENDS; j++) {
-                list.users[list.num].friends[j] = -1;
+                newuser.friends[j] = -1;
             }
-            list.num++;
+            FILE* file = fopen("../users.txt","a");
+            if (file == NULL) {
+                printf("No s'ha pogut obrir el fitxer");
+            }
+            else{
+                fprintf(file, "\n%s,%d,%s,%s,(%s,%s,%s,%s,%s)",newuser.user,newuser.age,newuser.mail,newuser.loc,newuser.hobby[0],newuser.hobby[1],newuser.hobby[2],newuser.hobby[3],newuser.hobby[4]);
+                fclose(file);
+                list = readUsersFromFile("../users.txt");
+            }
+
         }
         else if (opt == 2){
             for (int i = 0; i < list.num; i++){
-                if ( &list.users[i].user != -1){
-                    printf("%s\tEdat:%d\tHobbies:%s", &list.users[i].user, &list.users[i].age, &list.users[i].hobby);
-                }
+                printf("\n%s\tEdat:%d\tHobbies:%s,%s,%s,%s,%s", list.users[i].user, list.users[i].age, list.users[i].hobby[0],list.users[i].hobby[1],list.users[i].hobby[2],list.users[i].hobby[3],list.users[i].hobby[4]);
             }
             return 0;
         }
@@ -163,7 +109,7 @@ int menu(){
                 printf("Hobbies:%s,%s,%s,%s,%s\n", &list.users[found_user].hobby[0], &list.users[found_user].hobby[1], &list.users[found_user].hobby[2], &list.users[found_user].hobby[3], &list.users[found_user].hobby[4]);
                 int selected_user = found_user;
                 //Processos usuari
-                printf("\n1. Solicitud amistat\n2. Canviar d'usuari");
+                printf("\n1. Solicitud amistat\n2. Canviar d'usuari\n");
                 opt = -1;
                 while (opt == -1) {
                     if (opt == 1) {
@@ -206,9 +152,10 @@ int menu(){
 }
 
 int main() {
+    List list = readUsersFromFile("../users.txt");
     int stop = 0;
     while (stop==0) {
-        stop = menu();
+        stop = menu(list);
     }
     return 0;
 }
